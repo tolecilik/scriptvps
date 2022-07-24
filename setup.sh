13c8272d949279fea08f174d52584d264f7ce542
Uahiste <108900321+Uahiste@users.noreply.github.com>
2022-07-24 15:17:21 +0700

    Hso

--- setup.sh
+++ setup.sh
@@ -2,10 +2,7 @@
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
+
 fi
 # ==========================================
 # Color
